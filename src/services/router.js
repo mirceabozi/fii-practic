9c9ed1846d8101877cd9412d6cfc1370c5c5df20
Mircea Bozianu <mircea@spectrocloud.com.com>
2023-03-08 19:29:18 +0200

    Integrated React Router Dom + Created Register and Login files

---
+++ src/services/router.js
@@ -0,0 +1,22 @@
+
+import App from "../App"
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+  },
+  {
+    path:"/login",
+    element: <Login />,
+  },
+  {
+    path:"/register",
+    element: <Register/>,
+  }
+])
+
+export default router
