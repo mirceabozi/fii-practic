67f4df4eb7148ae14ba32cf445aa35dc83bb247e
Mircea Bozianu <mircea@spectrocloud.com.com>
2023-03-09 09:12:09 +0200

    Added Styled components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,40 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const NavBar = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  height: 80px;
+  align-items: center;
+
+  li{
+    list-style-type: none;
+    margin: 0 5px;
+  }
+`
+
 function Home() {
-  return <h1>HEI SUNT IN Home</h1>;
+  return (
+  <Layout>
+    <NavBar>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </NavBar>
+
+  </Layout>)
 }
 
 export default Home;
